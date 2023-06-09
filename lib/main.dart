efb566614233bbdfc539f228de0ded6a3f188027
Azis202017 <azisa6980@gmail.com>
2023-06-10 00:15:53 +0700

    [Main] added themeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
+import 'app/shared/theme/theme.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     GetMaterialApp(
       title: "Application",
-      initialRoute: AppPages.INITIAL,
+      initialRoute: AppPages.initial,
       getPages: AppPages.routes,
+      debugShowCheckedModeBanner: false,
+      theme: themeData,
     ),
   );
 }
