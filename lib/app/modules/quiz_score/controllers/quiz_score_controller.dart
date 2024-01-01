45424e6275fd51f957776690a3358423caaf5dae
Azis202017 <azisa6980@gmail.com>
2024-01-01 16:12:47 +0700

    [Quiz Score] added quiz score into application

--- lib/app/modules/quiz_score/controllers/quiz_score_controller.dart
+++ lib/app/modules/quiz_score/controllers/quiz_score_controller.dart
@@ -1,6 +1,33 @@
 import 'package:get/get.dart';
 
+import '../../../data/model/leaderboard_model.dart';
+import '../../../data/model/quiz_score_model.dart';
+import '../../../shared/services/leaderboard_service.dart';
+
 class QuizScoreController extends GetxController {
-  int score = Get.arguments['score'];
+  QuizScore score = Get.arguments['score'];
+  String type = Get.arguments['type'];  
+  String id = Get.arguments['materialId'];
+  List<Leaderboard> leaderboard = [];
+  bool isLoading = true;
   
+  @override
+  void onInit() {
+    fetchLeaderboardData();
+    super.onInit();
+  }
+  Future<void> fetchLeaderboardData() async {
+    print(id);
+    leaderboard = await LeaderboardService().fetchLeaderboardData(type: type,id: id);
+    isLoading  = false;
+    update(); 
+  }
+  int? searchStudentIdInLeaderboard(List<Leaderboard> leaderboardList) {
+    for (var leaderboard in leaderboardList) {
+      if (leaderboard.studentId != null) {
+        return leaderboard.rank;
+      }
+    }
+    return null;
+  }
 }
