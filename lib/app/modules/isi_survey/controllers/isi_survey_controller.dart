0477d2b050a1e67f019f99736c6575ae535acf3b
Azis202017 <azisa6980@gmail.com>
2024-01-01 16:15:44 +0700

    [Isi Survey] added isi survey into the application

---
+++ lib/app/modules/isi_survey/controllers/isi_survey_controller.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:get/get.dart';
+
+class IsiSurveyController extends GetxController {
+  InAppWebViewController? webViewController;
+  String? id = Get.arguments['id'];
+ 
+
+}
